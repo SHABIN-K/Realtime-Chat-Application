32f6ac45c01d4fb7e5de09615966ba9a358da9b1
Shabin-k <helloshabink@gmail.com>
2023-04-09 17:52:54 +0530

    router intergation copm

--- server/index.js
+++ server/index.js
@@ -3,9 +3,12 @@ const socketio = require("socket.io");
 const http = require("http");
 
 const PORT = process.env.PORT || 5000;
+const router = require('./Routes/router')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server has started`));
