6cf011e566d6c559f5562ea240c8ce001c3c79df
Shabin-k <helloshabink@gmail.com>
2023-04-09 17:00:59 +0530

    fix : fixed router promblem

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Chat from "./Components/Chat/Chat";
 import Join from "./Components/Join/Join";
 
-import { Route } from "react-router-dom";
-
 const App = () => {
   return (
-    <>
+    <Routes>
       <Route path="/" exact component={Join} />
       <Route path="/chat" component={Chat} />
-    </>
+    </Routes>
   );
 };
 
