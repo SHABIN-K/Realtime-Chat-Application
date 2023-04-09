eed6b4a1a317ddc179dbed84b89907438ff23616
Shabin-k <helloshabink@gmail.com>
2023-04-09 18:22:03 +0530

    Integrating Socket.IO

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ const socketio = require("socket.io");
 const http = require("http");
 
 const PORT = process.env.PORT || 5000;
-const router = require('./Routes/router')
+const router = require("./Routes/router");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.use(router);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(PORT, () => console.log(`Server has started`));
