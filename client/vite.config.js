f26356adf4f023c34a40fa5a65f7c7fd38ad096e
Shabin-k <helloshabink@gmail.com>
2023-04-10 17:09:29 +0530

    fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,45 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 import dotenv from "dotenv";
 
 export default defineConfig(({ mode }) => {
   dotenv.config({ mode });
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: {
+          enabled: true
+        },
+        injectRegister: 'auto',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+          cleanupOutdatedCaches: false
+        },
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+        manifest: {
+          name: 'Chat App',
+          short_name: 'Chat App',
+          description: 'Full stack real time chat application💖',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            }
+          ]
+        }
+      })
+    ],
     server: {
       port: 3000,
     },
