b348a32164c6afd60cf006f0fa0d6efd63a9f217
Shabin-k <helloshabink@gmail.com>
2023-04-15 16:58:38 +0530

    add version tag

--- client/src/Components/Join/Join.jsx
+++ client/src/Components/Join/Join.jsx
@@ -42,6 +42,8 @@ const Join = () => {
             Sign In
           </button>
         </Link>
+       <div className="vtag mt-20">version 2.3</div>
+       <div className="vtag ">Created by shabin k 😎</div>
       </div>
     </div>
   );
