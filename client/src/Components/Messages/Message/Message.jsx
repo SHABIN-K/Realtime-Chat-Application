36d122849fb522c417df3d0e6db2da40cfa597ac
Shabin-k <helloshabink@gmail.com>
2023-08-04 20:09:50 +0530

    feat : fix handle copy

--- client/src/Components/Messages/Message/Message.jsx
+++ client/src/Components/Messages/Message/Message.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-
 import "./Message.css";
-
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ReactEmoji from "react-emoji";
 
 const Message = ({ message: { text, user }, name }) => {
@@ -12,17 +12,24 @@ const Message = ({ message: { text, user }, name }) => {
     isSentByCurrentUser = true;
   }
 
+  const handleCopy = (text) => navigator.clipboard.writeText(text);
+
   return isSentByCurrentUser ? (
     <div className="messageContainer justifyEnd">
       <p className="sentText pr-10">{trimmedName}</p>
       <div className="messageBox backgroundBlue">
-        <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+        <p
+          className="messageText colorWhite"
+          onClick={() => handleCopy(text)}
+        >
+          {text}
+        </p>
       </div>
     </div>
   ) : (
     <div className="messageContainer justifyStart">
       <div className="messageBox backgroundLight">
-        <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+        <p className="messageText colorDark" onClick={() => handleCopy(text)}>{ReactEmoji.emojify(text)}</p>
       </div>
       <p className="sentText pl-10 ">{user}</p>
     </div>
