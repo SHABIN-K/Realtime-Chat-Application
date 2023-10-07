53c4208507114b8d0838bfb43f471e688f453e12
Shabin-k <helloshabink@gmail.com>
2023-10-07 18:55:35 +0530

    feat : change main section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,63 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import Chat from "./Components/Chat/Chat";
-import Join from "./Components/Join/Join";
-import ReloadChat from "./Components/Join/ReloadChat";
+import "./app.css";
+import Wtf from "./Components/contact/Wtf";
 
+import { pwaInfo } from 'virtual:pwa-info'
+import { runServer } from "./runServer";
+
+runServer();
+
+console.log(pwaInfo)
 const App = () => {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [error, setError] = useState("");
   return (
-    <Routes>
-      <Route path="/" element={<Join />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/reload-chat" element={<ReloadChat />} />
-    </Routes>
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room (number only)"
+            className="joinInput mt-20"
+            type="number"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        {error && <p className="error">{error}</p>}
+        <Link
+          onClick={(e) => {
+            if (!name || !room) {
+              e.preventDefault();
+              setError("Both fields are required.");
+            }
+          }}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className={"button mt-20"} type="submit">
+            Sign In
+          </button>
+        </Link>
+        <div className="vtag mt-20">version 1.0.6</div>
+        <div className="vtag ">
+          Created by{" "}
+          <a href="https://github.com/SHABIN-K" className="ctag">
+            shabin k 😎
+          </a>
+        </div>
+        <Wtf />
+      </div>
+    </div>
   );
 };
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import App from "./App";
+import Chat from "./Components/Chat/Chat";
+import ReloadChat from "./Components/Join/ReloadChat";
+
+import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/chat" element={<Chat />} />
+      <Route path="/reload-chat" element={<ReloadChat />} />
+    </Routes>
   </BrowserRouter>
   // </React.StrictMode>
 );
